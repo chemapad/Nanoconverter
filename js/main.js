54b3bfe71780004a2f66b696415536f96f8f7905
Alirio Mendez Jr <aliriomendezjr@gmail.com>
2017-12-16 22:37:18 -0430

    Select-option fixed and method "calculadora" restructured

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var btn = document.getElementById('btn');
 var preciodolar;
 var xrbpriceUSD;
 var xrbpricebitcoin;
@@ -6,64 +5,59 @@ var ethpricebitcoin;
 var bccpricebitcoin;
 var dogepricebitcoin;
 var eurpricebitcoin;
-var e = document.getElementById("opciones");
-var opcion = e.options[e.selectedIndex].value;
 
 
-var template = '<p>precio: :precio: <br> :preciousd:</p>' ;
 
-
-var coinmarketRequestxrb = new XMLHttpRequest ();
+var coinmarketRequestxrb = new XMLHttpRequest();
 coinmarketRequestxrb.open('GET', 'https://api.coinmarketcap.com/v1/ticker/Raiblocks/');
-coinmarketRequestxrb.onload = function(){
-	var ourdata= JSON.parse(coinmarketRequestxrb.responseText);
-  xrbpricebitcoin= parseFloat(ourdata[0].price_btc); 
-	xrbpriceUSD = parseFloat(ourdata[0].price_usd); 
-	 
+coinmarketRequestxrb.onload = function () {
+    var ourdata = JSON.parse(coinmarketRequestxrb.responseText);
+    xrbpricebitcoin = parseFloat(ourdata[0].price_btc);
+    xrbpriceUSD = parseFloat(ourdata[0].price_usd);
+
 };
 
-coinmarketRequestxrb.send(); 
+coinmarketRequestxrb.send();
 
 
-var coinmarketRequesteur = new XMLHttpRequest ();
+var coinmarketRequesteur = new XMLHttpRequest();
 coinmarketRequesteur.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice/EUR.json');
-coinmarketRequesteur.onload = function(){
-  var ourdata= JSON.parse(coinmarketRequesteur.responseText);
-  eurpricebitcoin= parseFloat(ourdata['bpi']['EUR'].rate_float);
-   
+coinmarketRequesteur.onload = function () {
+    var ourdata = JSON.parse(coinmarketRequesteur.responseText);
+    eurpricebitcoin = parseFloat(ourdata['bpi']['EUR'].rate_float);
+
 };
 
 coinmarketRequesteur.send();
 
 
-
-var coinmarketRequesteth = new XMLHttpRequest ();
+var coinmarketRequesteth = new XMLHttpRequest();
 coinmarketRequesteth.open('GET', 'https://api.coinmarketcap.com/v1/ticker/Ethereum/');
-coinmarketRequesteth.onload = function(){
-  var ourdata= JSON.parse(coinmarketRequesteth.responseText);
-  ethpricebitcoin= parseFloat(ourdata[0].price_btc);
-   
+coinmarketRequesteth.onload = function () {
+    var ourdata = JSON.parse(coinmarketRequesteth.responseText);
+    ethpricebitcoin = parseFloat(ourdata[0].price_btc);
+
 };
 
 coinmarketRequesteth.send();
 
-var coinmarketRequestbcc = new XMLHttpRequest ();
+var coinmarketRequestbcc = new XMLHttpRequest();
 coinmarketRequestbcc.open('GET', 'https://api.coinmarketcap.com/v1/ticker/Bitcoin-cash/');
-coinmarketRequestbcc.onload = function(){
-  var ourdata= JSON.parse(coinmarketRequestbcc.responseText);
-  bccpricebitcoin= parseFloat(ourdata[0].price_btc);
-   
+coinmarketRequestbcc.onload = function () {
+    var ourdata = JSON.parse(coinmarketRequestbcc.responseText);
+    bccpricebitcoin = parseFloat(ourdata[0].price_btc);
+
 };
 
 coinmarketRequestbcc.send();
 
 
-var coinmarketRequestdoge = new XMLHttpRequest ();
+var coinmarketRequestdoge = new XMLHttpRequest();
 coinmarketRequestdoge.open('GET', 'https://api.coinmarketcap.com/v1/ticker/dogecoin/');
-coinmarketRequestdoge.onload = function(){
-  var ourdata= JSON.parse(coinmarketRequestdoge.responseText);
-  dogepricebitcoin= parseFloat(ourdata[0].price_btc);
-   
+coinmarketRequestdoge.onload = function () {
+    var ourdata = JSON.parse(coinmarketRequestdoge.responseText);
+    dogepricebitcoin = parseFloat(ourdata[0].price_btc);
+
 };
 
 coinmarketRequestdoge.send();
@@ -72,157 +66,62 @@ coinmarketRequestdoge.send();
 var dolartodayRequest = new XMLHttpRequest();
 dolartodayRequest.open('GET', 'https://s3.amazonaws.com/dolartoday/data.json');
 dolartodayRequest.onload = function () {
-	var datadolar = JSON.parse(dolartodayRequest.responseText);
-	 preciodolar = datadolar.USD.bitcoin_ref;
-	calculadora();
+    var datadolar = JSON.parse(dolartodayRequest.responseText);
+    preciodolar = datadolar.USD.bitcoin_ref;
 }
 
 dolartodayRequest.send();
-
-function calculadora (dolarprecio, xrbprecio ){
-	var xrb= Number(preciodolar) * xrbpriceUSD;	
-	var $tvShowsContainer = $('#appdolar').find('#dolartoday');
-	/*$('#dolartoday')
-    .find('form')
-    .submit(function (ev) {
-      ev.preventDefault();*/
-      var busqueda = $('.input-group')
-        .find('input[type="number"]')
-        .val();
-        
-        var h=document.getElementById("boton");
-        h.addEventListener('click', function () {
-
-
-       
-
-
-        // Xrb-Bitcoin
-
-         /*if (opcion==1) {
-          var z= busqueda*xrbpricebitcoin;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z + ' BTC'; 
-        }*/
-        var e = document.getElementById("opciones");
-        var y = e.options[e.selectedIndex].value;
-
-        if (y==1) {
-            $(document).ready(function(){
-              $("form").submit(function(){
-                   var z= busqueda*xrbpricebitcoin;
-           
-            document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z + ' BTC'; 
-             });
-          });
-        }
-
-
-        e.addEventListener('change',
-            function(){    
-            var y = e.options[e.selectedIndex].value;
-          //XRB-BTC
-          if (y==1) {
-            $(document).ready(function(){
-              $("form").submit(function(){
-                   var z= busqueda*xrbpricebitcoin;
-           
-            document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z + ' BTC'; 
-             });
-          });
-        }
-
-        //Xrb-Eth
-        if (y==2) {
-          $(document).ready(function(){
-              $('form').submit(function(){
-                var xrbpriceeth= xrbpricebitcoin/ethpricebitcoin;
-                var z = busqueda*xrbpriceeth;
-                document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z +'ETH' ;
-             });
-          });
-         
-        }
-          
-         
-        
-        //XRB-BCH
-        if (y==3) {
-
-          var xrbpricebcc= xrbpricebitcoin/bccpricebitcoin;
-          var z = busqueda*xrbpricebcc;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z +'BCH' ;
-
-        }
-        //XRB-DOGE
-        if (y==4) {
-
-          var xrbpricedoge= xrbpricebitcoin/dogepricebitcoin;
-          var z = busqueda*xrbpricedoge;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z
-           +'DOGE' ;
-
-        }
-
-        // Xrb-Dollar
-        if (y==5) {
-          var x = busqueda * xrbpriceUSD;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ x + '$' ; 
-
-
-        }if (y==6) {
-          var x = xrbpricebitcoin * eurpricebitcoin;
-          var  y = busqueda * x;
-          //var z = accounting.formatMoney(y, "Bsf ", 2, ".", ",");
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ y + '€' ; 
-
-          //Xrb-Bolivar
-        }if (y==7) {
-            var x = busqueda * xrbpriceUSD;
-            var  y = busqueda * xrb;
-            var z = accounting.formatMoney(y, "Bsf ", 2, ".", ",");
-            document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z ; 
-        }
-             
-              });
-        
-
-      /* var article = template
-          .replace(':precio:', z)*/
-         
-       /* if (opcion==1) {
-          var z= busqueda*xrbpricebitcoin;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z + ' BTC';
-          var article = template
-          .replace(':precio:', z)
-        }
-         if (opcion==2) {
-          var xrbpriceeth= xrbpricebitcoin/ethpricebitcoin;
-          var z = busqueda*xrbpriceeth;
-          document.getElementById("lol").innerHTML = busqueda+' XRB '+ '  = '+ z +'ETH' ;
-          var article = template
-          .replace(':precio:', z)
-        }*/
-         
-         
-	  		
-       
-          
-         
-       
-       // document.getElementById("lol2").innerHTML = busqueda+' XRB '+ '  = ' + a; 
-        
-	
-
+var selected = 1; //bitcoin by default
+$('#opciones').on('change', function () {
+    selected = Number(this.value);
 });
 
 
-if (xrbpriceUSD==undefined) {
-	var intervalo= setInterval (calculadora, 500);
-}else {
-	document.getElementById("dolar").innerHTML = 'Coinmarketcap Raiblocks:' ;
-     document.getElementById("calendario").innerHTML = xrbpriceUSD+ '$ '  +'/ '+ xrbpricebitcoin+' BTC' ;
-} ;
+function calculadora() {
+    var xrb = Number(preciodolar) * xrbpriceUSD;
+    var amount = $('.input-group')
+        .find('input[type="number"]')
+        .val();
+    var html = amount + ' XRB ' + '  = ';
+    switch (selected) {
+        case 1:
+            //XRB-BTC
+            html+= (amount * xrbpricebitcoin) + ' BTC';
+            console.log(amount * xrbpricebitcoin);
+            break;
+        case 2:
+            //XRB-ETH
+            html += (amount * (xrbpricebitcoin / ethpricebitcoin)) + ' ETH';
+            break;
+        case 3:
+            //XRB-BCH
+            html += (amount * (xrbpricebitcoin / bccpricebitcoin)) + ' BCH';
+            break;
+        case 4:
+            //XRB-DOGE
+            html += (amount * (xrbpricebitcoin / dogepricebitcoin)) + ' DOGE';
+            break;
+        case 5:
+            //XRB-USD
+            html += (amount * xrbpriceUSD) + ' $';
+            break;
+        case 6:
+            //XRB-EUR
+            html += (amount * (xrbpricebitcoin * eurpricebitcoin)) + ' €'
+            break;
+        case 7:
+            //XRB-VEF
+            html += (accounting.formatMoney(amount * xrb, "Bsf ", 2, ".", ","))
+            break;
+    }
+    document.getElementById("lol").innerHTML = html;
 
-};
+    if (xrbpriceUSD == undefined) {
+        setInterval(calculadora, 500);
+    } else {
+        document.getElementById("dolar").innerHTML = 'Coinmarketcap Raiblocks:';
+        document.getElementById("calendario").innerHTML = xrbpriceUSD + '$ ' + '/ ' + xrbpricebitcoin + ' BTC';
+    }
+}
 
 
